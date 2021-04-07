8d82330829f24bffdd45d71f56e5ab45d8f27ab0
李国志 <guozhi.li@dmall.com>
2021-04-07 10:31:18 +0800

    css解析完成

--- src/watch/watch.rs
+++ src/watch/watch.rs
@@ -4,18 +4,20 @@ use std::hash::Hash;
 use std::path::Path;
 use std::time::SystemTime;
 
-trait WatchT<P: AsRef<Path> + Eq + Hash> {
+trait WatchT<P: AsRef<Path> + Eq + Hash + Clone> {
     fn add(&mut self, path: P) -> bool;
     fn remove(&mut self, path: P) -> bool;
 }
-#[derive(Debug)]
-struct Watch<P: AsRef<Path> + Eq + Hash> {
+#[derive(Debug, Default)]
+struct Watch<P: AsRef<Path> + Eq + Hash + Clone + Default> {
     dir: HashSet<P>,
     path_modified: HashMap<P, SystemTime>,
+    handle_pool: Vec<P>,
 }
 
-impl<P: AsRef<Path> + Eq + Hash> WatchT<P> for Watch<P> {
+impl<P: AsRef<Path> + Eq + Hash + Clone + Default> WatchT<P> for Watch<P> {
     fn add(&mut self, path: P) -> bool {
+        self.handle_pool.push(path.clone());
         self.dir.insert(path)
     }
 
@@ -23,19 +25,24 @@ impl<P: AsRef<Path> + Eq + Hash> WatchT<P> for Watch<P> {
         self.dir.remove(&path)
     }
 }
-impl<P: AsRef<Path> + Eq + Hash> Watch<P> {
+impl<P: AsRef<Path> + Eq + Hash + Clone + Default> Watch<P> {
     pub fn new() -> Self {
-        Watch {
-            dir: Default::default(),
-            path_modified: Default::default(),
-        }
+        Watch::default()
     }
 }
+impl<P: AsRef<Path> + Eq + Hash + Clone + Default> Iterator for Watch<P> {
+    type Item = P;
 
+    fn next(&mut self) -> Option<Self::Item> {
+        self.handle_pool.pop()
+    }
+}
 #[test]
 fn test_watch() {
     let mut w = Watch::new();
     w.add("./a");
     w.add("./b");
-    dbg!(w);
+    for i in w {
+        dbg!(i);
+    }
 }
