82ae23734b9204310a68a64125c13b34d1d3a41a
李国志 <guozhi.li@dmall.com>
2021-05-06 16:59:58 +0800

    import

--- src/config/config.rs
+++ src/config/config.rs
@@ -1,1 +1,5 @@
-
+use serde_derive::*;
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Config {
+    reg: String,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
+use clap::{App, Arg};
+use fcss::config::config::Config;
+use std::fs::File;
+use std::io::Read;
 
+fn main() {
+    let matches = App::new("fcss")
+        .version("1.0.1")
+        .author("jiuker")
+        .arg(
+            Arg::with_name("config")
+                .short("c")
+                .long("config")
+                .value_name("FILE")
+                .help("Sets a config file")
+                .takes_value(true),
+        )
+        .get_matches();
+    let config_path = matches
+        .value_of("config")
+        .unwrap_or("./res/test/config.json");
+    println!("load config {} file", config_path);
+    let mut config_file = File::open(config_path).unwrap();
+    let config: Config = serde_json::from_reader(config_file).unwrap();
 }
