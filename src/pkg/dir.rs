8bd72026a9cab1d93547ce0b841ecdbbcb5be4f4
李国志 <guozhi.li@dmall.com>
2021-05-10 16:19:19 +0800

    watch

--- src/pkg/dir.rs
+++ src/pkg/dir.rs
@@ -1,28 +1,51 @@
 use crate::pkg::result::CommonResult;
+use std::collections::hash_map::RandomState;
+use std::collections::{HashMap, HashSet};
 use std::fs::read_dir;
 use std::ops::Add;
 
-pub fn walk_all_dir(path: &String) -> CommonResult<Vec<String>> {
+pub fn walk_all_dir(
+    path: &String,
+) -> CommonResult<(Vec<String>, HashMap<String, HashSet<String>>)> {
     let current_list = std::fs::read_dir(path)?;
-    let mut paths: Vec<String> = vec![];
+    let mut dirs: Vec<String> = vec![];
+    let mut file_dirs: HashMap<String, HashSet<String>> = Default::default();
     for entry in current_list {
         let each_path = entry?;
         let x_meta = each_path.metadata()?;
         if x_meta.is_dir() {
-            for path in walk_all_dir(
-                &match each_path.path().to_str() {
-                    Some(d) => d,
-                    None => {
-                        return Err(Box::from("没有找到该目录"));
-                    }
-                }
-                .to_owned(),
-            )? {
-                paths.push(path);
+            let (child_dirs, child_file_dirs) =
+                walk_all_dir(&each_path.path().to_str().unwrap().to_owned())?;
+            for path in child_dirs {
+                dirs.push(path);
             }
-            paths.push(each_path.path().to_str().unwrap().to_string());
+            for f_d in child_file_dirs {
+                match file_dirs.get_mut(&f_d.0) {
+                    None => {
+                        let mut set = HashSet::new();
+                        set.extend(f_d.1);
+                        file_dirs.insert(f_d.0, set);
+                    }
+                    Some(d) => {
+                        d.extend(f_d.1);
+                    }
+                };
+            }
+            dirs.push(each_path.path().to_str().unwrap().to_string());
+        } else {
+            let file_name = each_path.file_name().to_str().unwrap().to_string();
+            match file_dirs.get_mut(&file_name) {
+                None => {
+                    let mut set = HashSet::new();
+                    set.insert(path.clone());
+                    file_dirs.insert(file_name, set);
+                }
+                Some(d) => {
+                    d.insert(path.clone());
+                }
+            };
         }
     }
-    paths.push(path.clone());
-    Ok(paths)
+    dirs.push(path.clone());
+    Ok((dirs, file_dirs))
 }
--- src/watch/watch.rs
+++ src/watch/watch.rs
@@ -1,7 +1,10 @@
+use crate::pkg::dir::walk_all_dir;
 use crate::pkg::result::CommonResult;
+use inotify::{EventMask, Inotify, WatchMask};
 use nom::dbg_dmp;
 use nom::lib::std::iter::FilterMap;
 use std::cell::RefCell;
+use std::collections::hash_map::RandomState;
 use std::collections::{HashMap, HashSet};
 use std::fmt::Debug;
 use std::fs::{read_dir, File, FileType, Metadata};
@@ -13,13 +16,12 @@ use std::sync::{Arc, Mutex};
 use std::thread::{sleep, spawn};
 use std::time::{Duration, SystemTime};
 
-#[derive(Debug)]
 pub struct Watch {
     file_type: String,
-    dir: Arc<Mutex<HashSet<String>>>,
-    path_modified: Arc<Mutex<HashMap<String, SystemTime>>>,
+    file_notify: Arc<Mutex<Inotify>>,
+    file_dirs: Arc<Mutex<HashMap<String, HashSet<String>>>>,
     sender: SyncSender<String>,
-    receiver: Arc<Mutex<Receiver<String>>>,
+    pub receiver: Arc<Mutex<Receiver<String>>>,
 }
 
 impl Watch {
@@ -27,78 +29,85 @@ impl Watch {
         let (sender, receiver) = sync_channel::<String>(10);
         Watch {
             file_type: t,
-            dir: Arc::new(Mutex::new(Default::default())),
-            path_modified: Arc::new(Mutex::new(Default::default())),
+            file_notify: Arc::new(Mutex::new(Inotify::init().unwrap())),
+            file_dirs: Default::default(),
             sender,
             receiver: Arc::new(Mutex::new(receiver)),
         }
     }
-    fn add(&self, path: String) -> bool {
-        self.dir.lock().unwrap().insert(path);
-        self.path_modified.lock().unwrap().clear();
-        for d in self.dir.lock().unwrap().iter() {
-            for (p, s) in self.walk(d).unwrap() {
-                match self.path_modified.lock().unwrap().insert(p, s) {
-                    _ => {}
-                };
-            }
+    pub fn add(&self, path: String) {
+        let (each_dirs, file_dirs) = walk_all_dir(&path).unwrap();
+        for each_dir in each_dirs {
+            self.file_notify
+                .lock()
+                .unwrap()
+                .add_watch(&each_dir, WatchMask::MODIFY | WatchMask::CREATE)
+                .expect("Failed to add inotify watch");
+            println!("Add dir {} to watch Success", each_dir)
         }
-        return true;
+        for f_d in file_dirs {
+            println!("Add file {} to watch Success", f_d.0.clone());
+            let mut file_dirs = self.file_dirs.lock().unwrap();
+            match file_dirs.get_mut(&f_d.0) {
+                None => {
+                    let mut set = HashSet::new();
+                    set.extend(f_d.1);
+                    file_dirs.insert(f_d.0, set);
+                }
+                Some(d) => {
+                    d.extend(f_d.1);
+                }
+            };
+        }
     }
 
-    fn remove(&mut self, path: String) -> bool {
-        self.dir.lock().unwrap().remove(&path)
-    }
-
-    fn watch(&self) {
+    pub fn watch(&self) {
+        let mut buffer = [0u8; 4096];
         loop {
-            sleep(Duration::new(1, 0));
-            {
-                for (p, s) in self.path_modified.lock().unwrap().iter_mut() {
-                    let ns = match self.read_file_modify_time(p) {
-                        Ok(d) => d,
-                        Err(_) => continue,
-                    };
-                    if !(*s).eq(&ns) {
-                        *s = ns;
-                        self.sender.send(p.clone());
+            let events = self
+                .file_notify
+                .lock()
+                .unwrap()
+                .read_events_blocking(&mut buffer)
+                .expect("Read File Events Error");
+            for event in events {
+                if event.mask.contains(EventMask::CREATE) || event.mask.contains(EventMask::MODIFY)
+                {
+                    if !event.mask.contains(EventMask::ISDIR) {
+                        match self
+                            .file_dirs
+                            .lock()
+                            .unwrap()
+                            .get(&*event.name.unwrap().to_str().unwrap().to_string())
+                        {
+                            None => {}
+                            Some(d) => {
+                                for dir in d.iter() {
+                                    let full_path =
+                                        Path::new(dir).join(event.name.unwrap().to_str().unwrap());
+                                    let m_time = full_path.metadata().unwrap().modified().unwrap();
+                                    if SystemTime::now()
+                                        .duration_since(m_time)
+                                        .unwrap()
+                                        .as_secs_f32()
+                                        < 1.0
+                                    {
+                                        self.sender.send(full_path.to_str().unwrap().to_string());
+                                        break;
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
         }
     }
-    fn read_file_modify_time(&self, p: &String) -> CommonResult<SystemTime> {
-        Ok(File::open(p)?.metadata()?.modified()?)
-    }
-    pub fn walk(&self, dir: &String) -> CommonResult<HashMap<String, SystemTime>> {
-        let mut h = HashMap::default();
-        let p_dir = read_dir(dir)?;
-        for p_file_or_dir in p_dir {
-            let d = p_file_or_dir?;
-            let m = d.metadata()?;
-            let p = match d.path().to_str() {
-                Some(d) => d,
-                None => continue,
-            }
-            .to_string();
-            if m.is_dir() {
-                for (p, t) in self.walk(&p)? {
-                    h.insert(p, t);
-                }
-            } else {
-                // 需要的才处理
-                if p.ends_with(&self.file_type) {
-                    h.insert(p, m.modified()?);
-                }
-            }
-        }
-        Ok(h)
-    }
 }
 
 #[test]
 fn test_watch() {
-    let mut w = Arc::new(Watch::new("vue".to_string()));
+    let w = Arc::new(Watch::new("vue".to_string()));
     w.add("/home/jiuker/goworkspace/src/dmallRedisSync/center/res/vue-element-admin".to_string());
     let w_c = w.clone();
     spawn(move || {
